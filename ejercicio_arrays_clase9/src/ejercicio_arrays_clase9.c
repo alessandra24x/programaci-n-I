9f7cb57efc2fccf8ab722f1b648f27cde0bda1b0
alumno <alumno@lab6pc00>
2019-09-19 22:14:49 -0300

    adding files

--- ejercicio_arrays_clase9/src/ejercicio_arrays_clase9.c
+++ ejercicio_arrays_clase9/src/ejercicio_arrays_clase9.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "utn.h"
  
-#define MAXRECORDS 3  //constante para el limite del arrays
+#define MAXRECORDS 4//constante para el limite del arrays
 typedef struct
 {
   char name[20];
   int dni;
   int age;
-  int isEmpty = 0;
-  int id = 0;
+  int isEmpty;
+ //int id = 0;
 }Student;
  
  
 int printMenu();
-int buscarPorDni (int a, Student *s) ;
-void mostrarAlumnos (Student * s);
+int buscarPorDni (int dni, Student *array) ;
+void mostrarAlumnos (Student * array);
  
  
-int main() {
-    Student array[MAXRECORDS], aux;
-    //struct Student arraySort[5], aux;
-    int age, dni, dniaux, i, j, options;
+int main(void) {
+	bool running = true;
+    Student array[MAXRECORDS];
+    int age, dni, options;
     char name[20];
+
+    for(int i=0; i < MAXRECORDS; ++i) {
+        array[i].isEmpty = 0; //La lista al principio está vacia
+    }
  
-    while(1) {
+    while(running) {
  
         options = printMenu(); //Se guarda en esta variable lo que retorna la función printMenu()
         switch(options) {
             case 1:
-                for(i = 0; i < MAXRECORDS; i++) {
-                    printf("///Ingrese los datos de la persona estudiante///\n");
+                for(int i = 0; i < MAXRECORDS; i++) {
+                	if(array[i].isEmpty == 0) {
+                    printf("///\nIngrese los datos de la persona estudiante///\n");
                     printf("Ingrese nombre: ");
                     fgets(name, sizeof(name), stdin);
                     getInt(&dni, "ingrese dni: ", "ingrese un dni valido", 1, 100000000, 3);
@@ -39,32 +45,33 @@ int main() {
                     strncpy(array[i].name, name, 20);
                     array[i].dni = dni;
                     array[i].age = age;
-                    //isEmpty++;
+                    array[i].isEmpty = 1;
+                	}
                 }
+                	if ((array[i].isEmpty = 0)) {
+                		printf("/// No hay espacio para más usuarios ///");
+                	}
                 break;
             case 2:
-                getInt(&id, "ingrese id: ", "ingrese un id valido", 1, 1000, 3);
-                i = buscarLibre(isEmptyAux, array);
+                getInt(&dni, "ingrese dni para la baja : ", "ingrese un dni valido", 1, 100000000, 3);
+                i = buscarPorDni(dni, array);
                 
                 if (i == -1)
                 {
                     printf ("El DNI ingresado no existe entre los datos. Presione cualquier tecla para continuar... \n");
-                    getchar();
                 }
                 else
                 {
-                	array[i].isEmpty = 1;
-                    return 0;
+                	array[i].isEmpty = 0;
                 }
                 break;
             case 3:
-                getInt(&dniaux, "ingrese dni: ", "ingrese un dni valido", 1, 100000000, 3);  //Se pide el dni a modificar
-                i = buscarPorDni(dniaux, array);
+                getInt(&dni, "ingrese dni: ", "ingrese un dni valido", 1, 100000000, 3);  //Se pide el dni a modificar
+                i = buscarPorDni(dni, array);
  
                 if (i == -1)
                 {
                     printf ("El DNI ingresado no existe entre los datos. Presione cualquier tecla para continuar... \n");
-                    getchar();
                 }
                 else
                 {
@@ -79,66 +86,68 @@ int main() {
                 mostrarAlumnos(array);
                 break;
             case 5:
-                for(i = 0; i < MAXRECORDS - 1; i++) {
-                    for(j = i+1; j < MAXRECORDS; j++) {
+                for(int i = 0; i < MAXRECORDS - 1; i++) {
+                    for(int j = i+1; j < MAXRECORDS; j++) {
                         //strcmp
                         //menor a cero = izquierda es menor derecha
                         //cero         = izquierda es igual que derecha
                         //mayor a cero = izquierda es mayor derecha
-                        if((strcmp(array[i].name,array[j].name))>0)
+                        if(strcmp(array[i].name,array[j].name) > 0)
                         {
-                            aux = array[i];
+                            Student aux = array[i];
                             array[i] = array[j];
                             array[j] = aux;
                         }
                     }
                 }
+                printf("\n/// La lista fue ordenada... ///");
                 break;
             case 6:
                 printf("Saliendo...");
-                return 0;
+                running = false;
+                break;
             default:
                 printf("Ingrese una opción valida\n");
-                break;
         }
-        system("clear");
     }
+    return 0;
 }
 
 // imprimo el menu en pantalla y el usuario selecciona la opción deseada
 int printMenu()    
 {
     int opcion; 
- 
+    printf("\n\n/// Menú ///\n");
     printf("1- Alta\n");
     printf("2- Baja\n");
     printf("3- Modificar\n");
     printf("4- Mostrar\n");
     printf("5- Ordenar\n");
     printf("6- Salir\n");
+    printf("Elija una opción: ");
     scanf("%d", &opcion);  //Se guarda la opción elegida por el usuario
- 
+    getchar(); //saco del buffer el enter...
     return opcion;    //Se retorna el valor elegido por el usuario
- 
+}
  
 /*
 con el DNi que ingresa el usuario se recorre el array.
 Si lo encuentra retorna la posicion donde lo encontró.
 Si no lo encuentra retorna -1 para poder controlar en la llamada a la función que hacer. 
 */
-int buscarPorDni(int dni, Student * alumnos)
+int buscarPorDni(int dni, Student * array)
 {
 int i, indice = -1;
  
-for (i = 0 ; i < MAXRECORDS ; i++)
-    if (alumnos[i].dni == dni)
+for (i = 0 ; i < MAXRECORDS && indice == -1 ; i++) {
+    if (array[i].dni == dni && array[i].isEmpty == 1) {
         indice = i;        //si lo encuentra se guarda el el lugar donde lo encontro. (siemore se queda con el ultimo)
- 
+    }
+}
 return indice;   //si no lo encuentra nunca en la recorrida retorna -1 que es el valor original
- 
 }
 
-int buscarLibre(int isEmpty, Student * alumnos)
+/*int buscarLibre(int isEmpty, Student * alumnos)
 {
 int i, indice = -1;
  
@@ -148,19 +157,20 @@ for (i = 0 ; i < MAXRECORDS ; i++)
  
 return indice;   //si no lo encontro nunca en la recorrida retorna -1 que es el valor original
  
-}
+}*/
  
-void mostrarAlumnos ( Student * alumnos)
-{
+void mostrarAlumnos ( Student * array) {
+
 int i;
- 
-for(i = 0; i < MAXRECORDS; i++)
-{
-    printf("\n Nombre: %s ", alumnos[i].name);
-    printf("\n DNI: %d ", alumnos[i].dni);
-    printf("\n Edad: %d ", alumnos[i].age);
+printf("\n/// Usuarios cargados ///\n");
+
+for(i = 0; i < MAXRECORDS; i++) {
+	if(array[i].isEmpty == 1) {
+    printf("\n Nombre: %s ", array[i].name);
+    printf("\n DNI: %d ", array[i].dni);
+    printf("\n Edad: %d ", array[i].age);
+	}
 }
 printf("\npresione cualquier tecla para continuar...");
 getchar(); //agregue esto para que despues de mostrar los datos del array haga una pausa
-getchar();
 }
