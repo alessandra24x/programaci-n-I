7bbdf0f3ee2ec220e04d0ab280a674e609ddac20
Alessandra Fernandes <41880854+alessandra24x@users.noreply.github.com>
2019-11-01 12:51:54 -0300

    Update Employee.c

--- tp3_linux_practica/Employee.c
+++ tp3_linux_practica/Employee.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "LinkedList.h"
 #include "Controller.h"
 #include "Employee.h"
 #include "utn.h"
+
+static int lastId = 0;
 
 Employee* employee_new() {
 	return (Employee*)malloc(sizeof(Employee));
@@ -49,18 +52,102 @@ int employee_setIdStr(Employee* this,char* idStr) {
 }
 
 int employee_setId(Employee* this,int id) {
-	int ret = -1;
-	if(this != NULL && id >= 0) {
-		this->id = id;
-		ret = 0;
-	}
-	return ret;
+    int ret = -1;
+    if(this != NULL && id >= 0) {
+      this->id = id;
+      ret = 0;
+    }
+    return ret;
 }
 
 int employee_getId(Employee* this,int* id) {
-	int ret = -1;
-	if(this != NULL && id != NULL) {
-		*id = this->id;
-	}
-	return ret;
+    int ret = -1;
+    if(this != NULL && id != NULL) {
+      *id = this->id;
+      ret = 0;
+    }
+    return ret;
+}
+
+int employee_setNombre(Employee* this,char* nombre) {
+    int ret = -1;
+    if(this != NULL && isValidName(nombre) == 0) {
+      strncpy(this->nombre,nombre,sizeof(this->nombre));
+      ret = 0;
+    }
+    return ret;
+}
+
+int employee_getNombre(Employee* this,char* nombre) {
+    int ret = -1;
+    if(this != NULL && nombre != NULL) {
+      strncpy(nombre,this->nombre,sizeof(this->nombre));
+      *nombre = this->id;
+      ret = 0;
+    }
+    return ret;
+}
+
+int employee_setHorasTrabajadasStr(Employee* this,char* horasTrabajadasStr) {
+    int ret = -1;
+    int auxHorasTrabajadas;
+    if(this != NULL && horasTrabajadasStr != NULL) {
+      if(isValidNumber(horasTrabajadasStr) == 0) {
+        auxHorasTrabajadas = atoi(horasTrabajadasStr);
+        if(employee_setHorasTrabajadas(this, auxHorasTrabajadas) == 0) {
+          ret = 0;
+        }
+      }
+    }
+    return ret;
+}
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas) {
+    int ret = -1;
+    if(this != NULL && horasTrabajadas >= 0) {
+      this->horasTrabajadas = horasTrabajadas;
+      ret = 0;
+    }
+    return ret;
+}
+
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas) {
+  int ret = -1;
+  if(this != NULL && horasTrabajadas != NULL) {
+    *horasTrabajadas = this->horasTrabajadas;
+    ret = 0;
+  }
+  return ret;
+}
+
+int employee_setSueldoStr(Employee* this,char* sueldoStr) {
+  int ret = -1;
+  int auxSueldo;
+  if(this != NULL && sueldoStr != NULL) {
+      if(isValidNumber(sueldoStr) == 0) {
+        auxSueldo = atoi(sueldoStr);
+        if(employee_setsueldo(this, auxSueldo) == 0) {
+          ret = 0;
+        }
+      }
+    }
+    return ret;
+}
+
+int employee_setSueldo(Employee* this,int sueldo) {
+  int ret = -1;
+  if(this != NULL && sueldo > 0) {
+    this->sueldo = sueldo;
+    ret = 0;
+  }
+  return ret;
+}
+
+int employee_getSueldo(Employee* this,int* sueldo) {
+  int ret = -1;
+  if(this != NULL && sueldo != NULL) {
+    *sueldo = this->sueldo;
+    ret = 0;
+  }
+  return ret;
 }
